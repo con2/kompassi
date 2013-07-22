7d59326be24eb6c3eb5fd8d970f2a6d49c86113b
Jussi Sorjonen <mieleton@mieleton.net>
2013-07-22 16:11:14 +0300

    Media and static paths

--- condb/settings.py
+++ condb/settings.py
@@ -1,5 +1,10 @@
 # Django settings for condb project.
 
+import os
+
+def mkpath(*parts):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '..', *parts))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -51,18 +56,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = mkpath('media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = mkpath('static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
