4b14d7d8231e8ba47f2f7db451284a431affef9d
Jussi Sorjonen <mieleton@mieleton.net>
2013-07-23 11:21:02 +0300

    Make template path platform-agnostic

--- condb/settings.py
+++ condb/settings.py
@@ -117,7 +117,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/mieleton/Work/condb/backend/templates',
+    mkpath('condb','templates'),
 )
 
 INSTALLED_APPS = (
