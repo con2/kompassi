a10f97c26fc995735efc4d517ce1f106c8563a2e
Jussi Sorjonen <mieleton@mieleton.net>
2013-07-23 20:24:06 +0300

    Added person roles, changed programme model etc.

--- backend/admin.py
+++ backend/admin.py
@@ -1,33 +1,39 @@
 import datetime
 from django.contrib import admin
-from backend.models import Category, Room, Person, Programme
+from backend.models import Category, Room, Person, Role, Programme
+
+class ProgramRoleInline(admin.TabularInline):
+    model = Programme.organizers.through
+    verbose_name = 'organizer'
+    verbose_name_plural = 'organizers'
 
 class PersonAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Basic information', {'fields': [('first_name','surname'),'nick']}),
         ('Contact information', {'fields': ['email','phone']}),
         ('Display', {'fields': ['anonymous']}),
+        ('Notes', {'fields': ['notes']}),
     ]
 
-    list_display = ('full_name','nick','email','phone')
+    list_display = ('full_name','email','phone','anonymous')
 
 class ProgrammeAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('Basic information', {'fields': ['title','hosts','description','category']}),
-        ('Time and location', {'fields': ['room',('start_time','end_time')]}),
+        ('Basic information', {'fields': ['title','description','category']}),
+        ('Time and location', {'fields': ['room',('start_time','length')]}),
         ('Display', {'fields': ['hilight','public']}),
+        ('Notes', {'fields': ['notes']}),
     ]
 
-    filter_horizontal = ('hosts',)
+    inlines = [
+        ProgramRoleInline,
+    ]
 
-    list_display = ('title','category','room','start_time','length')
+    list_display = ('title','category','room','start_time','length','end_time')
     list_filter = ('room','start_time','category')
-    
-    def length(self, obj):
-        return (obj.end_time - obj.start_time)
-    length.short_description = 'Length'
 
 admin.site.register(Category)
 admin.site.register(Room)
 admin.site.register(Person, PersonAdmin)
+admin.site.register(Role)
 admin.site.register(Programme, ProgrammeAdmin)
--- backend/models.py
+++ backend/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=1023)
     style = models.CharField(max_length=15)
+    notes = models.TextField(blank=True)
 
     def __unicode__(self):
         return self.title
@@ -13,11 +14,13 @@ class Category(models.Model):
         ordering = ['title']
         verbose_name_plural = "categories"
 
+
 # Room
 class Room(models.Model):
     name = models.CharField(max_length=1023)
     order = models.IntegerField(unique=True)
     public = models.BooleanField(default=True)
+    notes = models.TextField(blank=True)
 
     def __unicode__(self):
         return self.name
@@ -34,52 +37,75 @@ class Person(models.Model):
     email = models.EmailField(blank=True,max_length=254)
     phone = models.CharField(blank=True,max_length=255)
     anonymous = models.BooleanField()
+    notes = models.TextField(blank=True)
 
     @property
     def full_name(self):
-        return self.first_name + " " + self.surname
+        if self.nick:
+            return self.first_name + " \"" + self.nick + "\" " + self.surname
+        else:
+            return self.first_name + " " + self.surname
 
     @property
     def display_name(self):
         if self.anonymous:
             return self.nick
-        elif self.nick:
-            return self.first_name + " \"" + self.nick + "\" " + self.surname
         else:
-            return self.full_name  
+            return self.full_name
 
     def clean(self):
-        from django.core.exceptions import ValidationError
-        if not self.email or self.phone:
-            raise ValidationError('Either e-mail address or phone number must be provided')
         if self.anonymous and not self.nick:
+            from django.core.exceptions import ValidationError
             raise ValidationError('If real name is hidden a nick must be provided')
 
     def __unicode__(self):
-        return self.display_name
+        return self.full_name
 
     class Meta:
         ordering = ['surname']
 
+
+# Role
+class Role(models.Model):
+    title = models.CharField(max_length=1023)
+    require_contact_info = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+    
 
 # Programme
 class Programme(models.Model):
     title = models.CharField(max_length=1023)
     description = models.TextField()
     start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    length = models.IntegerField()
     hilight = models.BooleanField()
     public = models.BooleanField(default=True)
+    notes = models.TextField(blank=True)
     category = models.ForeignKey(Category)
     room = models.ForeignKey(Room)
-    hosts = models.ManyToManyField(Person)
+    organizers = models.ManyToManyField(Person, through='ProgramRole')
 
     @property
-    def length(self):
-        return (self.end_time - self.start_time)
+    def end_time(self):
+        return (self.start_time + datetime.timedelta(minutes = self.length))
 
     def __unicode__(self):
         return self.title
 
     class Meta:
         ordering = ['start_time','room']
+
+
+# ProgramRole
+class ProgramRole(models.Model):
+    person = models.ForeignKey(Person)
+    programme = models.ForeignKey(Programme)
+    role = models.ForeignKey(Role)
+
+    def __unicode__(self):
+        return self.role.title
