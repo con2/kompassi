c3b8986e54029e6c5f3a2a816b3ac608b2d5b476
Santtu Pajukanta <santtu@pajukanta.fi>
2013-08-14 17:51:20 +0300

    extranet schedule

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-from .views import timetable_view, internal_dumpdata_view
+from .views import timetable_view, internal_dumpdata_view, internal_timetable_view
 
 urlpatterns = patterns('',
     url(r'^$', timetable_view, name='timetable_view'),
+    url(r'^extranet/timetable$', internal_timetable_view, name='internal_timetable_view'),
     url(r'^data.json$', internal_dumpdata_view, name='internal_dumpdata_view'),
 )
--- timetable/views.py
+++ timetable/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 from .models import View, AllRoomsPseudoView
 
 def timetable_view(request):
+    return render_timetable(request, internal_programmes=False)
+
+def render_timetable(request, internal_programmes=False):
     all_rooms = AllRoomsPseudoView()
 
     vars = dict(
         views=View.objects.filter(public=True),
+        internal_programmes=internal_programmes,
         all_programmes_by_start_time=all_rooms.programmes_by_start_time
     )
 
     return render(request, 'timetable.jade', vars)
 
-@login_required
+@user_passes_test(lambda u: u.is_superuser)
 def internal_dumpdata_view(request):
     from django.core import management
     from django.http import HttpResponse
@@ -25,4 +29,7 @@ def internal_dumpdata_view(request):
     s.close()
 
     return r
-    
+
+#@login_required
+def internal_timetable_view(request):
+    return render_timetable(request, internal_programmes=True)
