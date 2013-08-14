ebf97d843f1fec7f67b1ace051339ae888e2a969
Santtu Pajukanta <santtu@pajukanta.fi>
2013-08-14 18:35:50 +0300

    legend

--- timetable/views.py
+++ timetable/views.py
@@ -2,18 +2,24 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.views.decorators.cache import cache_page, cache_control
 
-from .models import View, AllRoomsPseudoView
+from .models import View, AllRoomsPseudoView, Category, Tag
 
 @cache_control(public=True, max_age=5 * 60)
-@cache_page(5 * 60) # XXX remove once nginx cache is in place
+#@cache_page(5 * 60) # XXX remove once nginx cache is in place
 def timetable_view(request):
     return render_timetable(request, internal_programmes=False)
 
 def render_timetable(request, internal_programmes=False):
     all_rooms = AllRoomsPseudoView()
 
+    if internal_programmes:
+        category_query = dict()
+    else:
+        category_query = dict(public=True)
+
     vars = dict(
         views=View.objects.filter(public=True),
+        categories=Category.objects.filter(**category_query),
         internal_programmes=internal_programmes,
         all_programmes_by_start_time=all_rooms.programmes_by_start_time
     )
