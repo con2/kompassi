11ab7f428741ddfc29f8dd0179d0fee5b6f25fdf
Santtu Pajukanta <santtu@pajukanta.fi>
2014-01-05 19:29:13 +0200

    missing files

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.shortcuts import redirect
+
+from .views import *
+
+urlpatterns = patterns('',
+    url(r'^event/(?P<event>[a-z0-9-]+)', core_event_view),
+)
