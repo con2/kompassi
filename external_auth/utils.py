3982379f817d2941394da5daad8bdeae5a39e4af
Pekka Wallendahl <wyrmiyu@gmail.com>
2014-02-16 02:59:45 +0200

    Use a temporary password when a new user is created, add user to groups and then change the password to user supplied one.

--- external_auth/utils.py
+++ external_auth/utils.py
@@ -5,15 +5,25 @@ from django.conf import settings
 from . import ipa
 
 def create_user(user, password):
+    temporary_password = 'A very long 1-time temporary password'
+
     ipa.create_user(
         username=user.username,
         first_name=user.first_name,
         surname=user.last_name,
-        password=password,
+        password=temporary_password,
     )
 
     for group_name in settings.CONDB_NEW_USER_INITIAL_GROUPS:
         ipa.add_user_to_group(user.username, group_name)
+    
+    dn = 'uid=%s,%s' % (user.username, settings.CONDB_LDAP_USERS)
+    
+    ipa.change_user_password(
+        dn=dn,
+        old_password=temporary_password,
+        new_password=password,
+    )
 
 
 def change_current_user_password(request, old_password, new_password):
