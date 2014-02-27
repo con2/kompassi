76aabc7227cbec26ce02e7c0d7bba9d9b2080d9f
Santtu Pajukanta <santtu@pajukanta.fi>
2014-02-27 18:41:01 +0200

    fix ohjelma.tracon.fi redirect

--- programme/urls.py
+++ programme/urls.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 from .views import *
 
 urlpatterns = patterns('',
-    url(r'^events/(?P<event_id>[a-z0-9-]+)/timetable$', timetable_view, name='programme_timetable_view'),
+    url(r'^events/(?P<event_id>[a-z0-9-]+)/timetable/?$', timetable_view, name='programme_timetable_view'),
     url(r'^events/(?P<event_id>[a-z0-9-]+)/timetable/mobile/?$', mobile_timetable_view, name='programme_mobile_timetable_view'),
     url(r'^events/(?P<event_id>[a-z0-9-]+)/timetable/mobile/(\d{1,4})', mobile_programme_detail_view, name='mobile_programme_detail_view'),
     url(r'^events/(?P<event_id>[a-z0-9-]+)/timetable/full$', internal_timetable_view, name='programme_internal_timetable_view'),
