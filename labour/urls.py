4bcc3739d268dada59ee78ef0621c412df248cf9
Santtu Pajukanta <santtu@pajukanta.fi>
2014-03-30 11:27:58 +0300

    move labour admin from /events/tracon9/admin to /events/tracon9/labour/admin

--- labour/urls.py
+++ labour/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.shortcuts import redirect
+from django.views.generic.base import RedirectView
 
 from .views import *
 
@@ -12,48 +12,54 @@ urlpatterns = patterns('',
     url(r'^profile/qualifications/(?P<qualification>[a-z0-9-]+)/delete$', labour_person_disqualify_view, name='labour_person_disqualify_view'),
     url(r'^profile/qualifications/(?P<qualification>[a-z0-9-]+)/add$', labour_person_qualify_view, name='labour_person_qualify_view'),
 
-    url(r'^events/(?P<event_slug>[a-z0-9-]+)/admin$', labour_admin_dashboard_view, name='labour_admin_dashboard_view'),
-    url(r'^events/(?P<event_slug>[a-z0-9-]+)/admin/signups$', labour_admin_signups_view, name='labour_admin_signups_view'),
-    url(r'^events/(?P<event_slug>[a-z0-9-]+)/admin/signups/(?P<person_id>\d+)$', labour_admin_signup_view, name='labour_admin_signup_view'),
+    url(
+        r'^events/(?P<event_slug>[a-z0-9-]+)/admin(?P<suffix>.*)',
+        RedirectView.as_view(url='/events/%(event_slug)s/labour/admin%(suffix)s'),
+        name='labour_admin_old_urls_redirect'
+    ),
+
+    url(r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin$', labour_admin_dashboard_view, name='labour_admin_dashboard_view'),
+    url(r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/signups$', labour_admin_signups_view, name='labour_admin_signups_view'),
+    url(r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/signups/(?P<person_id>\d+)$', labour_admin_signup_view, name='labour_admin_signup_view'),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/admin/roster$',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/roster$',
         labour_admin_roster_view,
         name='labour_admin_roster_view'
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/admin/roster/jobcategories/(?P<job_category>\d+).json$',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/roster/jobcategories/(?P<job_category>\d+).json$',
         labour_admin_roster_job_category_fragment,
         name='labour_admin_roster_job_category_fragment'
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/admin/mail$',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/mail$',
         labour_admin_mail_view,
         name='labour_admin_mail_view'
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/admin/mail/new$',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/mail/new$',
         labour_admin_mail_editor_view,
         name='labour_admin_mail_new_view'
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/admin/mail/(?P<message_id>\d+)$',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/mail/(?P<message_id>\d+)$',
         labour_admin_mail_editor_view,
         name='labour_admin_mail_editor_view'
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/admin/mail/(?P<message_id>\d+)/preview',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/mail/(?P<message_id>\d+)/preview',
         labour_admin_mail_preview_view,
         name='labour_admin_mail_preview_view'
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/admin/mail/(?P<message_id>\d+)/preview/(?P<username>[a-z0-9_]+)',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/mail/(?P<message_id>\d+)/preview/(?P<username>[a-z0-9_]+)',
         labour_admin_mail_preview_fragment,
         name='labour_admin_mail_preview_fragment'
     ),
