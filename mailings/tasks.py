3824c404f82c02100fd93debed64b2dea3971d97
Santtu Pajukanta <santtu@pajukanta.fi>
2014-04-07 19:58:58 +0300

    fix missing Person import in mailings.tasks

--- mailings/tasks.py
+++ mailings/tasks.py
@@ -6,6 +6,7 @@ from celery import shared_task
 @shared_task(ignore_result=True)
 def message_send(message_id, recipient_ids=None, resend=False):
     from .models import Message
+    from core.models import Person
 
     message = Message.objects.get(pk=message_id)
     if recipient_ids is None:
