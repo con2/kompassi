63394277d4e04c5cd81ca1fc66f1d6d9c264b93a
Santtu Pajukanta <santtu@pajukanta.fi>
2014-08-25 16:25:27 +0300

    badges: wip

---
+++ badges/urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls import patterns, include, url
+from django.shortcuts import redirect
+
+from .views import (
+    badges_admin_index_view,
+    badges_admin_batches_view,
+    badges_admin_batch_view,
+)
+
+
+urlpatterns = patterns(
+    '',
+
+    url(
+        r'^events/(?P<event_slug>[a-z0-9-]+)/badges/admin/?$',
+        badges_admin_index_view,
+        name='badges_admin_index_view'
+    ),
+
+    url(
+        r'^events/(?P<event_slug>[a-z0-9-]+)/badges/admin/batches/?$',
+        badges_admin_batches_view,
+        name='badges_admin_index_view'
+    ),
+    
+    url(
+        r'^events/(?P<event_slug>[a-z0-9-]+)/badges/admin/batches/(?P<batch_id>\d+)?$',
+        badges_admin_batch_view,
+        name='badges_admin_index_view'
+    ),
+)
--- badges/views.py
+++ badges/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Badge, Batch
+from .helpers import badges_admin_required
+
+
+BADGE_ORDER = ('template', 'person__surname', 'person__first_name')
+
+
+@badges_admin_required
+def badges_admin_index_view(request, vars, event, badge_filter=None):
+    badge_criteria = dict(template__event=event)
+
+    if badge_filter is not None:
+        badge_criteria.update(template__slug=badge_filter)
+
+    vars.update(
+        badges=Badge.objects.filter(**badge_criteria).order_by(*BADGE_ORDER),
+    )
+
+    return render(request, 'badges_index_view', vars)
+
+
+@badges_admin_required
+def badges_admin_batches_view(request, vars, event):
+    raise NotImplemented()
+
+
+@badges_admin_required
+def badges_admin_batch_view(request, vars, event, batch_id):
+    raise NotImplemented()
+
+
+def badges_admin_menu_items(request, event):
+    menu_items = []
+
+    index_url = reverse('badges_admin_index_view', event.slug)
+    raise NotImplemented()
+
+    return menu_items
