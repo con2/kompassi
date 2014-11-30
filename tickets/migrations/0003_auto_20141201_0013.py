18f38c6b418875d531bbeca3f07c0ad0f9cb5349
Santtu Pajukanta <santtu@pajukanta.fi>
2014-12-01 00:14:51 +0200

    tickets: add missing migration

---
+++ tickets/migrations/0003_auto_20141201_0013.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tickets', '0002_ticketseventmeta_front_page_text'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='customer',
+            name='allow_marketing_email',
+            field=models.BooleanField(default=True, verbose_name='Minulle saa l\xe4hett\xe4\xe4 tapahtumaan liittyvi\xe4 tiedotteita s\xe4hk\xf6postitse'),
+        ),
+    ]
