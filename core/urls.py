f042f8ea69006d38c3e9fea8fdab9c808062dabb
Santtu Pajukanta <santtu@pajukanta.fi>
2015-03-10 19:10:56 +0200

    fix traconx.kompassi.eu etc. redirects

--- core/urls.py
+++ core/urls.py
@@ -24,7 +24,7 @@ urlpatterns = patterns(
     '',
 
     url(r'^$', core_frontpage_view, name='core_frontpage_view'),
-    url(r'^events/(?P<event_slug>[a-z0-9-]+)$', core_event_view, name='core_event_view'),
+    url(r'^events/(?P<event_slug>[a-z0-9-]+)/?$', core_event_view, name='core_event_view'),
     url(r'^login$', core_login_view, name='core_login_view'),
     url(r'^logout$', core_logout_view, name='core_logout_view'),
     url(r'^register$', core_registration_view, name='core_registration_view'),
