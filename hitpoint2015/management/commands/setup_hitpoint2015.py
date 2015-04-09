e2555987119853820c1bc7c8078f1acab5ccb379
Santtu Pajukanta <santtu@pajukanta.fi>
2015-04-09 15:01:50 +0300

    hitpoint2015: initialize tickets module

--- hitpoint2015/management/commands/setup_hitpoint2015.py
+++ hitpoint2015/management/commands/setup_hitpoint2015.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+import os
 from datetime import datetime, timedelta
 
 from django.core.management.base import BaseCommand, make_option
@@ -8,6 +9,10 @@ from django.utils.timezone import now
 from dateutil.tz import tzlocal
 
 from core.utils import slugify
+
+
+def mkpath(*parts):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', *parts))
 
 
 class Setup(object):
@@ -23,6 +28,8 @@ class Setup(object):
         self.tz = tzlocal()
         self.setup_core()
         self.setup_labour()
+        self.setup_tickets()
+        self.setup_payments()
 
     def setup_core(self):
         from core.models import Venue, Event
@@ -235,6 +242,94 @@ class Setup(object):
                     group=labour_event_meta.get_group(link_group),
                 )
             )
+
+    def setup_tickets(self):
+        from tickets.models import TicketsEventMeta, LimitGroup, Product
+
+        tickets_admin_group, unused = TicketsEventMeta.get_or_create_group(self.event, 'admins')
+
+        defaults = dict(
+            admin_group=tickets_admin_group,
+            due_days=14,
+            shipping_and_handling_cents=120,
+            reference_number_template="2015{:05d}",
+            contact_email='Tracon Hitpoint -lipunmyynti <hitpoint@tracon.fi.fi>',
+            plain_contact_email='hitpoint@tracon.fi.fi',
+            ticket_free_text=u"Tämä on sähköinen lippusi Tracon Hitpoint -tapahtumaan. Sähköinen lippu vaihdetaan rannekkeeseen\n"
+                u"lipunvaihtopisteessä saapuessasi tapahtumaan. Voit tulostaa tämän lipun tai näyttää sen\n"
+                u"älypuhelimen tai tablettitietokoneen näytöltä. Mikäli kumpikaan näistä ei ole mahdollista, ota ylös\n"
+                u"kunkin viivakoodin alla oleva neljästä tai viidestä sanasta koostuva Kissakoodi ja ilmoita se\n"
+                u"lipunvaihtopisteessä.\n\n"
+                u"Tervetuloa Tracon Hitpointiin!",
+            front_page_text=u"<h2>Tervetuloa ostamaan pääsylippuja Tracon Hitpoint -tapahtumaan!</h2>"
+                u"<p>Liput maksetaan suomalaisilla verkkopankkitunnuksilla heti tilauksen yhteydessä.</p>"
+                u"<p>Lue lisää tapahtumasta <a href='http://2015.hitpoint.tracon.fi'>Tracon Hitpoint -tapahtuman kotisivuilta</a>.</p>"
+                u"<p>Huom! Tämä verkkokauppa palvelee ainoastaan asiakkaita, joilla on osoite Suomessa. Mikäli tarvitset "
+                u"toimituksen ulkomaille, ole hyvä ja ota sähköpostitse yhteyttä: <em>hitpoint@tracon.fi</em>"
+        )
+
+        if self.test:
+            t = now()
+            defaults.update(
+                ticket_sales_starts=t - timedelta(days=60),
+                ticket_sales_ends=t + timedelta(days=60),
+            )
+        else:
+            # TODO
+            # defaults.update(
+            #     ticket_sales_starts=datetime(2015, 3, 4, 18, 0, tzinfo=self.tz),
+            #     ticket_sales_ends=datetime(2015, 6, 28, 18, 0, tzinfo=self.tz),
+            # )
+            pass
+
+        meta, unused = TicketsEventMeta.objects.get_or_create(event=self.event, defaults=defaults)
+
+        def limit_group(description, limit):
+            limit_group, unused = LimitGroup.objects.get_or_create(
+                event=self.event,
+                description=description,
+                defaults=dict(limit=limit),
+            )
+
+            return limit_group
+
+        for product_info in [
+            # dict(
+            #     name=u'Tracon Hitpoint -pääsylippu',
+            #     description=u'TODO',
+            #     limit_groups=[
+            #         limit_group('Pääsyliput', 3000),
+            #     ],
+            #     price_cents=1600,
+            #     requires_shipping=True,
+            #     electronic_ticket=False,
+            #     available=True,
+            #     ordering=self.get_ordering_number(),
+            # ),
+        ]:
+            name = product_info.pop('name')
+            limit_groups = product_info.pop('limit_groups')
+
+            product, unused = Product.objects.get_or_create(
+                event=self.event,
+                name=name,
+                defaults=product_info
+            )
+
+            if not product.limit_groups.exists():
+                product.limit_groups = limit_groups
+                product.save()
+
+        # v5
+        if not meta.print_logo_path:
+            meta.print_logo_path = mkpath('static', 'images', 'hitpoint.png')
+            meta.print_logo_width_mm = 30
+            meta.print_logo_height_mm = 30
+            meta.save()
+
+    def setup_payments(self):
+        from payments.models import PaymentsEventMeta
+        PaymentsEventMeta.get_or_create_dummy(event=self.event)
 
 
 class Command(BaseCommand):
