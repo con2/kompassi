c098a9550a03dec49508c1d496f615c393bbda14
Santtu Pajukanta <santtu@pajukanta.fi>
2015-08-06 23:54:49 +0300

    badges: fix job title handling in default factory

--- badges/utils.py
+++ badges/utils.py
@@ -8,17 +8,20 @@ def default_badge_factory(event, person):
     Returns a dictionary that can be fed into the constructor of badges.models:Badge.
     """
 
+    job_title = u''
+
     if event.labour_event_meta is not None:
         from labour.models import Signup
 
         try:
             signup = Signup.objects.get(event=event, person=person)
         except Signup.DoesNotExist:
-            job_title = u''
+            pass
         else:
-            job_title = signup.job_title or u''
-    else:
-        job_title = u''
+            if signup.job_title:
+                job_title = signup.job_title
+            elif signup.job_categories_accepted.exists():
+                job_title = signup.job_categories_accepted.first().name
 
     return dict(
         personnel_class=signup.personnel_classes.order_by('priority').first(),
