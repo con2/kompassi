3f186294a020f6a3aa25b1b88e166c7aea13a251
Jyrki Launonen <jyrki.launonen@gmail.com>
2015-08-13 19:04:23 +0300

    core, labour: Add some sanity checking for person birth date.
    - Rough field validation,
    - use fallback format when formatting date before computers.

---
+++ core/migrations/0003_auto_20150813_1907.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import core.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0002_auto_20150126_1611'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='person',
+            name='birth_date',
+            field=models.DateField(blank=True, help_text='Syntym\xe4aika muodossa 24.2.1994', null=True, verbose_name='Syntym\xe4aika', validators=[core.models.birth_date_validator]),
+            preserve_default=True,
+        ),
+    ]
