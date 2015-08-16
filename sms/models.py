eac9d357066235e285349c5f280dbb3a03277bf1
Miika Ojamo <miika@vkoski.net>
2015-08-16 16:40:38 +0300

    Property-model for sms-enabled events

--- sms/models.py
+++ sms/models.py
@@ -184,6 +184,10 @@ class SMSEvent(models.Model):
                 pass
         return super(SMSEvent, self).save(*args, **kwargs)
 
+    @property
+    def is_sms_enabled(self):
+        return self.sms_enabled is not None
+
     def __unicode__(self):
         return self.event
 
