a73adb8d4c8b766858560ac233e37562c17bc12d
Santtu Pajukanta <santtu@pajukanta.fi>
2015-10-11 20:05:21 +0300

    membership: model field help text changes

---
+++ core/migrations/0013_auto_20151011_2005.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0012_auto_20151011_1926'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='person',
+            name='first_name',
+            field=models.CharField(max_length=1023, verbose_name='Etunimi'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='person',
+            name='muncipality',
+            field=models.CharField(help_text='Virallinen kotikuntasi eli kunta jossa olet kirjoilla. Kotikunta ja v\xe4est\xf6rekisteriin merkityt etunimesi (kaikki) ovat pakollisia tietoja, mik\xe4li kuulut tai haluat liitty\xe4 johonkin yhdistykseen joka k\xe4ytt\xe4\xe4 Kompassin kehitys\xadinstanssia\xa0j\xe4senrekisterin hallintaan.', max_length=127, verbose_name='Kotikunta', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='person',
+            name='official_first_names',
+            field=models.CharField(max_length=1023, verbose_name='Viralliset etunimet', blank=True),
+            preserve_default=True,
+        ),
+    ]
---
+++ membership/migrations/0006_auto_20151011_2005.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('membership', '0005_membership_message'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='membership',
+            name='organization',
+            field=models.ForeignKey(related_name='members', verbose_name='Organisaatio', to='core.Organization'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='membership',
+            name='person',
+            field=models.ForeignKey(related_name='memberships', verbose_name='Henkil\xf6', to='core.Person'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='membershiporganizationmeta',
+            name='membership_fee',
+            field=models.TextField(help_text='Mink\xe4 suuruinen on liittymis- ja j\xe4senmaksu ja miten se maksetaan?', verbose_name='J\xe4senmaksu', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='membershiporganizationmeta',
+            name='membership_requirements',
+            field=models.TextField(help_text='Esim. copy-paste s\xe4\xe4nn\xf6ist\xe4.', verbose_name='Kuka voi hakea j\xe4senyytt\xe4?', blank=True),
+            preserve_default=True,
+        ),
+    ]
