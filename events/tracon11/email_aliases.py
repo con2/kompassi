c2f946baaa9ec3b24d5d51bfb9bfc1cdcf13097d
Santtu Pajukanta <santtu@pajukanta.fi>
2015-10-27 10:13:08 +0200

    access: final minor fix!

--- events/tracon11/email_aliases.py
+++ events/tracon11/email_aliases.py
@@ -4,7 +4,7 @@ from labour.models import Signup
 
 
 def requested_alias_or_nick(person):
-    signup = Signup.objects.filter(event__slug='tracon11', person=person)
+    signup = Signup.objects.filter(event__slug='tracon11', person=person).first()
     if signup:
         if signup.signup_extra.email_alias:
             return emailify(signup.signup_extra.email_alias)
