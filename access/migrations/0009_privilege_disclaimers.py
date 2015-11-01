6dc6e4c3bd0a119ba7448c0d1949f3e381e00b94
Santtu Pajukanta <santtu@pajukanta.fi>
2015-11-01 17:57:45 +0200

    access: missing migration

---
+++ access/migrations/0009_privilege_disclaimers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('access', '0008_smtp'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='privilege',
+            name='disclaimers',
+            field=models.TextField(blank=True),
+            preserve_default=True,
+        ),
+    ]
