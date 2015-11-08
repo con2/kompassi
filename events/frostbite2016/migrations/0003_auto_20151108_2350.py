5362d3570e07d0be3bc3ecbabf2b6bc96a7fbb70
Santtu Pajukanta <santtu@pajukanta.fi>
2015-11-09 00:00:04 +0200

    missing migration

---
+++ events/frostbite2016/migrations/0003_auto_20151108_2350.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('frostbite2016', '0002_desu_amount_must_be_natural'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='signupextra',
+            name='shirt_type',
+            field=models.CharField(default='STAFF', max_length=8, verbose_name='Paidan tyyppi', choices=[('STAFF', 'Staff'), ('DESURITY', 'Desurity'), ('KUVAAJA', 'Kuvaaja'), ('VENDOR', 'Myynti')]),
+        ),
+    ]
