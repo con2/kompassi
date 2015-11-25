3714cf42d899f8f7d0c6f57862c793218d816834
Santtu Pajukanta <santtu@pajukanta.fi>
2015-11-25 18:04:37 +0200

    membership: mass approve membership requests

--- membership/views/admin_views.py
+++ membership/views/admin_views.py
@@ -2,9 +2,10 @@
 
 from django.contrib import messages
 from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.timezone import now
-from django.http import HttpResponse
+from django.views.decorators.http import require_safe, require_http_methods
 
 from api.utils import handle_api_errors, api_login_required
 from core.csv_export import csv_response, CSV_EXPORT_FORMATS
@@ -26,6 +27,7 @@ EXPORT_FORMATS = [
 
 
 @membership_admin_required
+@require_http_methods(['GET', 'HEAD', 'POST'])
 def membership_admin_members_view(request, vars, organization, format='screen'):
     memberships = organization.memberships.all().select_related('person')
     num_all_members = memberships.count()
@@ -38,6 +40,11 @@ def membership_admin_members_view(request, vars, organization, format='screen'):
     ])
 
     memberships = memberships.order_by('person__surname', 'person__official_first_names')
+
+    if request.method == 'POST' and state_filters.selected_slug == 'approval':
+        memberships.update(state='in_effect')
+        messages.success(request, u'Hyväksyntää odottavat jäsenhakemukset hyväksyttiin.')
+        return redirect('membership_admin_members_view', organization.slug)
 
     export_type = state_filters.selected_slug
     if export_type == 'approval':
@@ -56,6 +63,7 @@ def membership_admin_members_view(request, vars, organization, format='screen'):
     )
 
     vars.update(
+        show_approve_all_button=state_filters.selected_slug == 'approval',
         memberships=memberships,
         num_members=memberships.count(),
         num_all_members=num_all_members,
@@ -85,6 +93,7 @@ def membership_admin_members_view(request, vars, organization, format='screen'):
         )
 
 @membership_admin_required
+@require_http_methods(['GET', 'HEAD', 'POST'])
 def membership_admin_member_view(request, vars, organization, person_id):
     membership = get_object_or_404(Membership, organization=organization, person=int(person_id))
     read_only = membership.person.user is not None
@@ -142,6 +151,7 @@ def membership_admin_member_view(request, vars, organization, person_id):
 
 @handle_api_errors
 @api_login_required
+@require_safe
 def membership_admin_emails_api(request, organization_slug):
     organization = get_object_or_404(Organization, slug=organization_slug)
 
