7f82e592eafab6bdb1f72ba5ba27bc7a8b1edd76
Santtu Pajukanta <santtu@pajukanta.fi>
2016-01-29 18:10:31 +0200

    badges: use raw_id_fields for high-cardinality tables in admin

--- badges/admin.py
+++ badges/admin.py
@@ -12,6 +12,7 @@ class BadgeAdmin(admin.ModelAdmin):
     list_display = ('event_name', 'person_full_name', 'personnel_class_name', 'batch')
     list_filter = ('personnel_class__event',)
     search_fields = ('person__surname', 'person__first_name', 'person__nick', 'person__email')
+    raw_id_fields = ('person', 'batch', 'personnel_class')
 
 
 admin.site.register(Badge, BadgeAdmin)
