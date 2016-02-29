a9d19f29123db82489eb605524eb490d9a3dcb78
Santtu Pajukanta <santtu@pajukanta.fi>
2016-02-29 21:32:43 +0200

    desuprofile_integration: improved admin

--- desuprofile_integration/admin.py
+++ desuprofile_integration/admin.py
@@ -5,11 +5,14 @@ from .models import Connection, ConfirmationCode
 
 class ConnectionAdmin(admin.ModelAdmin):
     model = Connection
+    list_display = ('user', 'desuprofile_username')
     raw_id_fields = ('user',)
+    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'user__email', 'desuprofile_username')
 
 
 class ConfirmationCodeAdmin(admin.ModelAdmin):
     model = ConfirmationCode
+    list_display = ('person', 'desuprofile_username')
     raw_id_fields = ('person',)
 
 
