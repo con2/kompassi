4c7c6ecfc84f94ada4639f97fa7ea03b9c522959
Santtu Pajukanta <santtu@pajukanta.fi>
2016-04-27 23:38:12 +0300

    labour/roster: fix empty allocated/required in job category view

--- labour/static_src/viewmodels/RequirementCell.js
+++ labour/static_src/viewmodels/RequirementCell.js
@@ -7,8 +7,8 @@ export default class RequirementCell {
     this.jobCategory = jobCategory;
     this.job = job; // might be null
     this.startTime = startTime;
-    this.allocated = allocated;
-    this.required = required;
+    this.allocated = allocated || 0;
+    this.required = required || 0;
   }
 
   edit() {
