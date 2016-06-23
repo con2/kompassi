151153f6f9f1af9abc779af23775d8cd431d3b28
Santtu Pajukanta <santtu@pajukanta.fi>
2016-06-24 01:23:01 +0300

    programme: missing import

--- programme/models/schedule.py
+++ programme/models/schedule.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 import logging
 from datetime import timedelta
 
+from django.contrib import messages
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
