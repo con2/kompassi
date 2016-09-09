77b406c693dd00f40f71b9fcd851ac485539030c
Santtu Pajukanta <santtu@pajukanta.fi>
2016-09-09 21:19:33 +0300

    tracon11: for afterparty signup, ban most popular bus choices… unless they have already signed up for it

--- events/tracon11/forms.py
+++ events/tracon11/forms.py
@@ -237,6 +237,21 @@ class AfterpartyParticipationSurvey(forms.ModelForm):
         self.helper = horizontal_form_helper()
         self.helper.form_tag = False
 
+        # Ban most popular bus choices… unless they have already signed up for it
+        if self.instance.outward_coach_departure_time != '16:00':
+            self.fields['outward_coach_departure_time'].choices = [
+                (id, text)
+                for id, text in self.fields['outward_coach_departure_time'].choices
+                if id != '16:00'
+            ]
+
+        if self.instance.return_coach_departure_time != '01:00':
+            self.fields['return_coach_departure_time'].choices = [
+                (id, text)
+                for id, text in self.fields['return_coach_departure_time'].choices
+                if id != '01:00'
+            ]
+
     @classmethod
     def get_instance_for_event_and_person(cls, event, person):
         return SignupExtraV2.objects.get(
