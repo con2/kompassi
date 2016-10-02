6659bb2a71eb03b79e2033a47ec8eabba5a1c7e9
Meepu <meeri.panula@gofore.com>
2016-10-02 20:10:31 +0300

    Check if already enrolled

--- enrollment/views/public_views.py
+++ enrollment/views/public_views.py
@@ -3,11 +3,19 @@ from core.helpers import person_required
 from django.shortcuts import render
 
 from ..helpers import enrollment_event_required
+from ..models import Enrollment
 
 @enrollment_event_required
 @person_required
 def enrollment_enroll_view(request, event_slug):
 
-    vars = dict()
+    already_enrolled = Enrollment.objects.filter(
+        event=event_slug,
+        person=request.user.person,
+    ).exists()
+
+    vars = dict(
+        already_enrolled=already_enrolled,
+    )
 
     return render(request, 'enrollment_enroll_view.jade', vars)
