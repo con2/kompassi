1f2bf6136189827652fbbf8f916398ded897a289
Santtu Pajukanta <santtu@pajukanta.fi>
2016-10-21 16:33:34 +0300

    kontena: the experiment

--- turska/wsgi.py
+++ turska/wsgi.py
@@ -2,4 +2,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "turska.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling, MediaCling
+
+
+application = Cling(MediaCling(get_wsgi_application()))
