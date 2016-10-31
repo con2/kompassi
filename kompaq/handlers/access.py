d26a21035f9d93ccd0338536c5552489f7571154
Santtu Pajukanta <santtu@pajukanta.fi>
2016-10-31 22:39:15 +0200

    kompaq: add a note about currently not needing a post_delete hook for SMTPPassword

--- kompaq/handlers/access.py
+++ kompaq/handlers/access.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals, absolute_import
 
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 
 from access.models import SMTPPassword
 
@@ -16,3 +16,9 @@ def on_smtppassword_created(sender, instance, created, **kwargs):
         return
 
     send_update(instance, 'created')
+
+
+# NOTE not currently needed because UI does not have a feature for this
+# @receiver(post_delete, sender=SMTPPassword)
+def on_smtppassword_deleted(sender, instance, **kwargs):
+    send_update(instance, 'deleted')
