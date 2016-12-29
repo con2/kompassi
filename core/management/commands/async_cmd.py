4e29c6706c75d0f1c43ab4b8a825f998075f1951
Santtu Pajukanta <santtu@pajukanta.fi>
2016-12-29 14:24:55 +0200

    core/management/commands/async_cmd: run admin commands in celery

---
+++ core/management/commands/async_cmd.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Run an admin command in Celery'
+
+    def add_arguments(self, parser):
+        parser.add_argument('async_cmd_args', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        assert 'background_tasks' in settings.INSTALLED_APPS
+
+        print options
+
+        from core.tasks import run_admin_command
+        run_admin_command.delay(*options['async_cmd_args'])
--- core/tasks.py
+++ core/tasks.py
@@ -4,6 +4,7 @@ import logging
 
 from django.conf import settings
 from django.core.mail import EmailMessage
+from django.core.management import call_command
 
 from celery import shared_task
 
@@ -31,3 +32,8 @@ def person_apply_state_new_user_async(person_id, password):
     from .models import Person
     person = Person.objects.get(id=person_id)
     person._apply_state_new_user_async(password)
+
+
+@shared_task(ignore_result=True)
+def run_admin_command(*args, **kwargs):
+    call_command(*args, **kwargs)
