76afc67caffb0088d70abea6a31b602af338e8e1
Santtu Pajukanta <santtu@pajukanta.fi>
2017-01-14 17:14:33 +0200

    manage.py: speed up exit under docker

--- manage.py
+++ manage.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 import os
 import sys
+import signal
+
+
+def sighandler(signum, frame):
+    """
+    Speed up exit under Docker.
+    http://blog.lotech.org/fix-djangos-runserver-when-run-under-docker-or-pycharm.html
+    """
+    sys.exit(0)
+
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGTERM, sighandler)
+    signal.signal(signal.SIGINT, sighandler)
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "turska.settings")
 
     from django.core.management import execute_from_command_line
