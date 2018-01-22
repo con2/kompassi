e32cb3efa99aa280568051e3ee848a5c0142e330
Santtu Pajukanta <santtu@pajukanta.fi>
2018-01-22 22:01:40 +0200

    Revert "core/login: exempt successful login requests from CSP :("
    This reverts commit 69b08ff27c2d4e0ebe4940a84ec2ed9af9c01642.

--- core/views/login_views.py
+++ core/views/login_views.py
@@ -84,10 +84,7 @@ def do_login(request, user, password=None, next='core_frontpage_view'):
 
     remind_email_verification_if_needed(request, next)
 
-    # FIXME CSP breaks login redirects to OAuth2 enabled sites. Find a way to make them co-exist.
-    response = redirect(next)
-    response._csp_exempt = True
-    return response
+    return redirect(next)
 
 
 @require_http_methods(['GET', 'HEAD', 'POST'])
