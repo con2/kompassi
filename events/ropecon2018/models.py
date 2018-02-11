0e96db151ab95a12f928432125aa3bb1b3e8ab8b
Kalle Kivimaa <killer@basen.net>
2018-02-11 13:23:03 +0200

    Initial Ropecon 2018 setup

---
+++ events/ropecon2018/models.py
@@ -0,0 +1,136 @@
+# encoding: utf-8
+
+
+
+from django.db import models
+
+from labour.models import ObsoleteSignupExtraBaseV1
+from labour.querybuilder import QueryBuilder, add_prefix
+
+
+TOTAL_WORK_CHOICES = [
+    ('8h', 'Minimi - 8 tuntia'),
+    ('12h', '10–12 tuntia'),
+    ('yli12h', 'Työn Sankari! Yli 12 tuntia!'),
+]
+
+EXTRA_WORK_CHOICES = [
+    ('miel', 'Otan mielelläni varatyövoimavuoron'),
+    ('ei', 'En halua varatyövuoroa'),
+]
+
+SHIFT_TYPE_CHOICES = [
+    ('yksipitka', 'Yksi pitkä vuoro'),
+    ('montalyhytta', 'Monta lyhyempää vuoroa'),
+    ('kaikkikay', 'Kumpi tahansa käy'),
+]
+
+class SimpleChoice(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
+
+
+class SpecialDiet(SimpleChoice):
+    id = models.AutoField(auto_created=True, 
+        primary_key=True, 
+        serialize=False, 
+        verbose_name='ID')
+    
+    name = models.CharField(max_length=63)
+
+    class Meta:
+        abstract = False
+
+class TimeSlot(SimpleChoice):
+    id = models.AutoField(auto_created=True, 
+        primary_key=True, 
+        serialize=False, 
+        verbose_name='ID')
+    
+    name = models.CharField(max_length=63)
+
+    class Meta:
+        abstract = False
+
+class SignupExtra(ObsoleteSignupExtraBaseV1):
+    shift_type = models.CharField(max_length=15,
+        verbose_name='Toivottu työvuoron pituus',
+        help_text='Haluatko tehdä yhden pitkän työvuoron vaiko monta lyhyempää vuoroa?',
+        choices=SHIFT_TYPE_CHOICES,
+    )
+
+    total_work = models.CharField(max_length=15,
+        verbose_name='Toivottu kokonaistyömäärä',
+        help_text='Kuinka paljon haluat tehdä töitä yhteensä tapahtuman aikana? Useimmissa tehtävistä minimi on kahdeksan tuntia, mutta joissain tehtävissä se voi olla myös vähemmän (esim. majoitusvalvonta 6 h).',
+        choices=TOTAL_WORK_CHOICES,
+    )
+
+    extra_work = models.CharField(max_length=5,
+        verbose_name='Haluatko päivystää varatyövoimana?',
+        help_text='Varatyövoiman päivystysvuoro on 4h, jonka aikana sinun tulee olla tavoitettavissa puhelimella Messukeskuksessa. Voit joutua mihin tahansa työpisteeseen paikkaamaan',
+        choices=EXTRA_WORK_CHOICES,
+    )
+
+    want_certificate = models.BooleanField(
+        default=False,
+        verbose_name='Haluan todistuksen työskentelystäni Ropeconissa',
+    )
+
+    certificate_delivery_address = models.TextField(
+        blank=True,
+        verbose_name='Työtodistuksen toimitusosoite',
+        help_text='Jos haluat työtodistuksen, täytä tähän kenttään postiosoite (katuosoite, '
+            'postinumero ja postitoimipaikka) johon haluat todistuksen toimitettavan.',
+    )
+
+    special_diet = models.ManyToManyField(
+        SpecialDiet,
+        blank=True,
+        verbose_name='Erikoisruokavalio'
+    )
+
+    special_diet_other = models.TextField(
+        blank=True,
+        verbose_name='Muu erikoisruokavalio',
+        help_text='Jos noudatat erikoisruokavaliota, jota ei ole yllä olevassa listassa, '
+            'ilmoita se tässä. Tapahtuman järjestäjä pyrkii ottamaan erikoisruokavaliot '
+            'huomioon, mutta kaikkia erikoisruokavalioita ei välttämättä pystytä järjestämään.'
+    )
+
+    prior_experience = models.TextField(
+        blank=True,
+        verbose_name='Työkokemus',
+        help_text='Kerro tässä kentässä, jos sinulla on aiempaa kokemusta vastaavista '
+            'tehtävistä tai muuta sellaista työkokemusta, josta arvioit olevan hyötyä '
+            'hakemassasi tehtävässä.'
+    )
+
+    shift_wishes = models.TextField(
+        blank=True,
+        verbose_name='Alustavat työvuorotoiveet',
+        help_text='Jos tiedät nyt jo, ettet pääse paikalle johonkin tiettyyn aikaan tai haluat '
+            'osallistua johonkin tiettyyn ohjelmanumeroon, mainitse siitä tässä.'
+    )
+
+    free_text = models.TextField(
+        blank=True,
+        verbose_name='Vapaa alue',
+        help_text='Jos haluat sanoa hakemuksesi käsittelijöille jotain sellaista, jolle ei ole '
+            'omaa kenttää yllä, käytä tätä kenttää.'
+    )
+
+    is_active = models.BooleanField(default=True)
+    
+    @classmethod
+    def get_form_class(cls):
+        from .forms import SignupExtraForm
+        return SignupExtraForm
+
+    @staticmethod
+    def get_query_class():
+        raise NotImplementedError()
