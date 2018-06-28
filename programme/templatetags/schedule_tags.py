536187940bf3d2d7fd46081836b1dace0b03449c
Santtu Pajukanta <santtu@pajukanta.fi>
2018-06-28 09:41:33 +0300

    programme: hide empty hours in the programme listing
    A horribly convoluted way of hiding empty headings in the programme listings
    such as the one below the programme schedule table.
    
    The listing is implemented by using the same programme data as the table does.
    Due to this, the data contains all kinds of elements unnecessary to the flat
    listing:
    
    [(start_time, incontinuity, programmes)]
    
    where `programmes` is a list of (programme, rowspan).
    
    The list of (programme, rowspan) contains (None, None) for each empty cell
    in the schedule. This is why a simple `if programmes` will not do to hide
    the heading.
    
    The correct condition is `any(prog for (prog, rowspan) in programmes)` which,
    unfortunately, Django templates will not let us embed in the template code
    itself due to Hitler and the Nazis.
    
    So we use an inclusion tag itself, computing the aforementioned condition
    safely here in the Python code for the template to simply `if` on it.

---
+++ programme/templatetags/schedule_tags.py
@@ -0,0 +1,38 @@
+from django.template import Library
+
+
+__all__ = ['programme_list_heading']
+register = Library()
+
+
+@register.inclusion_tag('programme_list_heading.jade')
+def programme_list_heading(start_time, programmes):
+    """
+    Used by programme_schedule_list.jade alone.
+
+    A horribly convoluted way of hiding empty headings in the programme listings
+    such as the one below the programme schedule table.
+
+    The listing is implemented by using the same programme data as the table does.
+    Due to this, the data contains all kinds of elements unnecessary to the flat
+    listing:
+
+    [(start_time, incontinuity, programmes)]
+
+    where `programmes` is a list of (programme, rowspan).
+
+    The list of (programme, rowspan) contains (None, None) for each empty cell
+    in the schedule. This is why a simple `if programmes` will not do to hide
+    the heading.
+
+    The correct condition is `any(prog for (prog, rowspan) in programmes)` which,
+    unfortunately, Django templates will not let us embed in the template code
+    itself due to Hitler and the Nazis.
+
+    So we use an inclusion tag itself, computing the aforementioned condition
+    safely here in the Python code for the template to simply `if` on it.
+    """
+    return dict(
+        start_time=start_time,
+        should_render=any(prog for (prog, rowspan) in programmes),
+    )
