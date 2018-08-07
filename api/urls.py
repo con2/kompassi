75117dd681eb99ddcbd04f895252302f09795310
Santtu Pajukanta <santtu@pajukanta.fi>
2018-08-07 07:41:29 +0300

    api: fix status endpoint url

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from .views import api_person_view, api_status_view
 
 urlpatterns = [
     url(r'^api/v1/people/(?P<username>[a-zA-Z0-9_]+)/?$', api_person_view, name='api_person_view'),
-    url('r^api/v1/status/?$', api_status_view, name='api_status_view'),
+    url(r'^api/v1/status/?$', api_status_view, name='api_status_view'),
 ]
