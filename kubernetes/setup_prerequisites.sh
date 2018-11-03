ba52166a8c99fd2655e74be28fa1ea70721d5644
Santtu Pajukanta <santtu@pajukanta.fi>
2018-11-03 16:31:03 +0200

    kubernetes: use con2 version of secret-generator

--- kubernetes/setup_prerequisites.sh
+++ kubernetes/setup_prerequisites.sh
@@ -2,8 +2,8 @@
 set -xue
 
 # secret-generator
-kubectl apply -f https://raw.githubusercontent.com/mittwald/kubernetes-secret-generator/master/deploy/secret-generator-rbac.yaml
-kubectl apply -f https://raw.githubusercontent.com/mittwald/kubernetes-secret-generator/master/deploy/secret-generator.yaml
+kubectl apply -f https://raw.githubusercontent.com/con2/kubernetes-secret-generator/con2/deploy/secret-generator-rbac.yaml
+kubectl apply -f https://raw.githubusercontent.com/con2/kubernetes-secret-generator/con2/deploy/secret-generator.yaml
 
 # ingress-nginx
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
