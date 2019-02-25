1ec983c0e4658ec06c0159c5d4eeab6557ed5e29
Santtu Pajukanta <santtu@pajukanta.fi>
2019-02-25 12:10:51 +0200

    ropeconjvp2019: wording

--- events/ropeconjvp2019/forms.py
+++ events/ropeconjvp2019/forms.py
@@ -20,7 +20,7 @@ class EnrollmentForm(forms.ModelForm):
             self.fields[field_name].required = True
 
         self.fields['traconjv_avow_and_affirm'].label = 'Vakuutan olevani lain tarkoittamalla tavalla rehellinen ja luotettava ja henkilökohtaisilta ominaisuuksiltani tehtävään sopiva, eikä minulla ole voimassaolevia tai vanhoja tuomioita tai rikosrekisteriä.'
-        self.fields['traconjv_solemnly_swear'].label = 'Vakuutan antamani tiedot oikeiksi. Ilmoittautumalla sitoudun osallistumaan peruskurssille 25.–31.3. pääkaupunkiseudulla ja järjestyksenvalvojana Ropecon 2019- tai Ropecon 2020-tapahtumiin.'
+        self.fields['traconjv_solemnly_swear'].label = 'Vakuutan antamani tiedot oikeiksi. Ilmoittautumalla sitoudun osallistumaan peruskurssille 25.–31.3. pääkaupunkiseudulla ja järjestyksenvalvojana Ropecon 2019- ja Ropecon 2020 -tapahtumiin.'
 
     class Meta:
         model = Enrollment
