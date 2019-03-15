ccc2996cb8098be17479b375e2b2d49e3428842f
Santtu Pajukanta <santtu@pajukanta.fi>
2019-03-15 20:30:36 +0200

    continue forms implementation (serializers etc)

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
 
+from core.utils import slugify, validate_slug
+
 from .models import Event
+
+
+class SlugDefaultor:
+    def __init__(self, source_field='name'):
+        self.source_field = source_field
+
+    def set_context(self, field):
+        self.source_value = getattr(field.parent.instance, seflf.source_field)
+
+    def __call__(self):
+        return slugify(self.source_value)
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
@@ -16,4 +29,8 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
             'headline',
             'homepage_url',
         )
+        read_only_fields = (
+            'slug',
+        )
+
         model = Event
