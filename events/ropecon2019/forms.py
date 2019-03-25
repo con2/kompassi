c9cd8ad4dbbd2e058edd121f68557995f7d3ecae
Santtu Pajukanta <santtu@pajukanta.fi>
2019-03-25 21:06:26 +0200

    ropecon2019: fix conitealomake

--- events/ropecon2019/forms.py
+++ events/ropecon2019/forms.py
@@ -5,6 +5,7 @@ from crispy_forms.layout import Layout, Fieldset
 
 from core.forms import horizontal_form_helper
 from labour.forms import AlternativeFormMixin
+from labour.models import Signup, JobCategory
 from programme.forms import AlternativeProgrammeFormMixin
 from programme.models import Category, Programme
 
@@ -465,3 +466,73 @@ class GamingDeskForm(forms.ModelForm, AlternativeProgrammeFormMixin):
         widgets = dict(
             ropecon2019_preferred_time_slots=forms.CheckboxSelectMultiple,
         )
+
+
+class OrganizerSignupForm(forms.ModelForm, AlternativeFormMixin):
+    def __init__(self, *args, **kwargs):
+        event = kwargs.pop('event')
+        admin = kwargs.pop('admin')
+
+        assert not admin
+
+        super(OrganizerSignupForm, self).__init__(*args, **kwargs)
+
+        self.helper = horizontal_form_helper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Fieldset('Tehtävän tiedot',
+                'job_title',
+            ),
+        )
+
+        self.fields['job_title'].help_text = "Mikä on tehtäväsi coniteassa? Printataan badgeen."
+        self.fields['job_title'].required = True
+
+    class Meta:
+        model = Signup
+        fields = ('job_title',)
+
+        widgets = dict(
+            job_categories=forms.CheckboxSelectMultiple,
+        )
+
+    def get_excluded_m2m_field_defaults(self):
+        return dict(
+            job_categories=JobCategory.objects.filter(event__slug='ropecon2019', name='Conitea')
+        )
+
+
+class OrganizerSignupExtraForm(forms.ModelForm, AlternativeFormMixin):
+    def __init__(self, *args, **kwargs):
+        super(OrganizerSignupExtraForm, self).__init__(*args, **kwargs)
+        self.helper = horizontal_form_helper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Fieldset('Lisätiedot',
+                'special_diet',
+                'special_diet_other',
+            ),
+        )
+
+    class Meta:
+        model = SignupExtra
+        fields = (
+            'special_diet',
+            'special_diet_other',
+        )
+
+        widgets = dict(
+            special_diet=forms.CheckboxSelectMultiple,
+        )
+
+    def get_excluded_field_defaults(self):
+        return dict(
+            shift_type='kaikkikay',
+            want_certificate=False,
+            certificate_delivery_address='',
+            prior_experience='',
+            free_text='Syötetty käyttäen coniitin ilmoittautumislomaketta',
+        )
+
+    def get_excluded_m2m_field_defaults(self):
+        return dict()
