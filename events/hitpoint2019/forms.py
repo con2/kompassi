8b51cd3f674a9b356ced01ef9bd66521a360c48e
Santtu Pajukanta <santtu@pajukanta.fi>
2019-08-05 07:40:00 +0300

    hitpoint2019: fix 500 on programme forms more

--- events/hitpoint2019/forms.py
+++ events/hitpoint2019/forms.py
@@ -163,7 +163,7 @@ APPROXIMATE_LENGTH_HELP_TEXT = _(
 
 class RpgForm(forms.ModelForm, AlternativeProgrammeFormMixin):
     def __init__(self, *args, **kwargs):
-        kwargs.pop('admin') if admin in kwargs
+        kwargs.pop('admin') if 'admin' in kwargs else False
         kwargs.pop('event')
 
         super(RpgForm, self).__init__(*args, **kwargs)
@@ -236,7 +236,7 @@ class FreeformForm(forms.ModelForm, AlternativeProgrammeFormMixin):
     """
 
     def __init__(self, *args, **kwargs):
-        kwargs.pop('admin') if admin in kwargs
+        kwargs.pop('admin') if 'admin' in kwargs else False
         kwargs.pop('event')
 
         super(FreeformForm, self).__init__(*args, **kwargs)
