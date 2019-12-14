a5543283ad652e776c962aa61da82724a25679ba
Santtu Pajukanta <santtu@pajukanta.fi>
2019-12-14 18:19:02 +0200

    forms/FormResponse: readonly created_at, updated_at

--- forms/serializers/form_response.py
+++ forms/serializers/form_response.py
@@ -24,4 +24,8 @@ class FormResponseSerializer(serializers.HyperlinkedModelSerializer):
             'created_at',
             'updated_at',
         )
+        read_only_fields = (
+            'created_at',
+            'updated_at',
+        )
         model = FormResponse
