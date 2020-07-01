14f936b903412d314ea1d98cd491e34c444f6027
Santtu Pajukanta <santtu@pajukanta.fi>
2020-07-01 08:35:23 +0300

    labour: add optional slash to urls

--- labour/urls.py
+++ labour/urls.py
@@ -34,15 +34,15 @@ urlpatterns = [
     url(r'^events/(?P<event_slug>[a-z0-9-]+)/signup/?$', labour_signup_view, name='labour_signup_view'),
     url(r'^events/(?P<event_slug>[a-z0-9-]+)/signup/(?P<alternative_form_slug>[a-z0-9-]+)/?$', labour_signup_view, name='labour_special_signup_view'),
 
-    url(r'^profile/qualifications$', labour_qualifications_view, name='labour_qualifications_view'),
-    url(r'^profile/qualifications/(?P<qualification>[a-z0-9-]+)$', labour_person_qualification_view, name='labour_person_qualification_view'),
+    url(r'^profile/qualifications/?$', labour_qualifications_view, name='labour_qualifications_view'),
+    url(r'^profile/qualifications/(?P<qualification>[a-z0-9-]+)/?$', labour_person_qualification_view, name='labour_person_qualification_view'),
 
     # XXX make these DELETE and POST of labour_person_qualification_view
-    url(r'^profile/qualifications/(?P<qualification>[a-z0-9-]+)/delete$', labour_person_disqualify_view, name='labour_person_disqualify_view'),
-    url(r'^profile/qualifications/(?P<qualification>[a-z0-9-]+)/add$', labour_person_qualify_view, name='labour_person_qualify_view'),
+    url(r'^profile/qualifications/(?P<qualification>[a-z0-9-]+)/delete/?$', labour_person_disqualify_view, name='labour_person_disqualify_view'),
+    url(r'^profile/qualifications/(?P<qualification>[a-z0-9-]+)/add/?$', labour_person_qualify_view, name='labour_person_qualify_view'),
 
     url(
-        r'^profile/signups$',
+        r'^profile/signups/?$',
         labour_profile_signups_view,
         name='labour_profile_signups_view',
     ),
@@ -66,25 +66,25 @@ urlpatterns = [
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin$',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/?$',
         labour_admin_dashboard_view,
         name='labour_admin_dashboard_view'
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/signups$',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/signups/?$',
         labour_admin_signups_view,
         name='labour_admin_signups_view'
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/signups\.(?P<format>xlsx|csv|tsv|html)$',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/signups\.(?P<format>xlsx|csv|tsv|html)/?$',
         labour_admin_signups_view,
         name='labour_admin_export_view'
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/signups/(?P<person_id>\d+)$',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/signups/(?P<person_id>\d+)/?$',
         labour_admin_signup_view,
         name='labour_admin_signup_view'
     ),
@@ -127,19 +127,19 @@ urlpatterns = [
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/mail$',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/mail/?$',
         labour_admin_mail_view,
         name='labour_admin_mail_view'
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/mail/new$',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/mail/new/?$',
         labour_admin_mail_editor_view,
         name='labour_admin_mail_new_view'
     ),
 
     url(
-        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/mail/(?P<message_id>\d+)$',
+        r'^events/(?P<event_slug>[a-z0-9-]+)/labour/admin/mail/(?P<message_id>\d+)/?$',
         labour_admin_mail_editor_view,
         name='labour_admin_mail_editor_view'
     ),
