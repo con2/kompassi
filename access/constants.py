84ae93e01240854cb1e965245ddcf15dbd105b5b
Santtu Pajukanta <santtu@pajukanta.fi>
2021-02-20 15:15:58 +0200

    access: make event-specific access last 180 days after event instead of 90

--- access/constants.py
+++ access/constants.py
@@ -1,5 +1,5 @@
 # event manager permissions are set to expire this many days after event ends
-CBAC_VALID_AFTER_EVENT_DAYS = 90
+CBAC_VALID_AFTER_EVENT_DAYS = 180
 
 # when a superuser overrides permissions, this is how many minutes the temporary permissions last
 CBAC_SUDO_VALID_MINUTES = 20
