9b78e2291d63f65a1b73566f6a35a895096e6c53
Santtu Pajukanta <santtu@pajukanta.fi>
2022-05-16 12:14:05 +0300

    programme: add may_send_info to ProgrammeRole csv export

--- programme/models/programme_role.py
+++ programme/models/programme_role.py
@@ -7,31 +7,32 @@ from core.csv_export import CsvExportMixin
 
 
 class ProgrammeRole(models.Model, CsvExportMixin):
-    person = models.ForeignKey('core.Person', on_delete=models.CASCADE, related_name='programme_roles')
-    programme = models.ForeignKey('programme.Programme', on_delete=models.CASCADE)
-    role = models.ForeignKey('programme.Role', on_delete=models.CASCADE)
-    invitation = models.ForeignKey('programme.Invitation', on_delete=models.CASCADE, null=True, blank=True)
+    person = models.ForeignKey("core.Person", on_delete=models.CASCADE, related_name="programme_roles")
+    programme = models.ForeignKey("programme.Programme", on_delete=models.CASCADE)
+    role = models.ForeignKey("programme.Role", on_delete=models.CASCADE)
+    invitation = models.ForeignKey("programme.Invitation", on_delete=models.CASCADE, null=True, blank=True)
 
     # denormalized from programme.state
     is_active = models.BooleanField(default=True)
 
     extra_invites = models.PositiveIntegerField(
         default=0,
-        verbose_name=_('Extra invites'),
-        help_text=_('The host may send this many extra invites to other hosts of the programme.'),
+        verbose_name=_("Extra invites"),
+        help_text=_("The host may send this many extra invites to other hosts of the programme."),
     )
 
     def clean(self):
         if self.role.require_contact_info and not (self.person.email or self.person.phone):
             from django.core.exceptions import ValidationError
-            raise ValidationError('Contacts of this type require some contact info')
+
+            raise ValidationError("Contacts of this type require some contact info")
 
     def __str__(self):
-        return '{person} ({programme})'.format(person=self.person, programme=self.programme)
+        return "{person} ({programme})".format(person=self.person, programme=self.programme)
 
     class Meta:
-        verbose_name = _('Programme host')
-        verbose_name_plural = _('Programme hosts')
+        verbose_name = _("Programme host")
+        verbose_name_plural = _("Programme hosts")
 
     @classmethod
     def from_invitation(cls, invitation, person):
@@ -44,7 +45,7 @@ class ProgrammeRole(models.Model, CsvExportMixin):
             programme=invitation.programme,
             role=invitation.role,
             invitation=invitation,
-            extra_invites=invitation.extra_invites
+            extra_invites=invitation.extra_invites,
         )
 
     @classmethod
@@ -66,7 +67,7 @@ class ProgrammeRole(models.Model, CsvExportMixin):
             programme=programme,
             defaults=dict(
                 role=role,
-            )
+            ),
         )
 
         programme.apply_state()
@@ -78,7 +79,7 @@ class ProgrammeRole(models.Model, CsvExportMixin):
         if self.extra_invites < 1:
             return text_type(self.extra_invites)
         else:
-            return '{invites_used}/{invites}'.format(
+            return "{invites_used}/{invites}".format(
                 invites_used=self.extra_invites_used,
                 invites=self.extra_invites,
             )
@@ -86,6 +87,7 @@ class ProgrammeRole(models.Model, CsvExportMixin):
     @property
     def extra_invites_used(self):
         from .invitation import Invitation
+
         return self.sired_invitation_set.count()
 
     @property
@@ -94,7 +96,7 @@ class ProgrammeRole(models.Model, CsvExportMixin):
 
     @property
     def role_or_status(self):
-        if self.programme.state in ['accepted', 'published']:
+        if self.programme.state in ["accepted", "published"]:
             return self.role.title
         else:
             return self.programme.get_state_display()
@@ -107,15 +109,16 @@ class ProgrammeRole(models.Model, CsvExportMixin):
         from .role import Role
 
         return [
-            (Person, 'surname'),
-            (Person, 'first_name'),
-            (Person, 'nick'),
-            (Person, 'email'),
-            (Person, 'phone'),
-            (Programme, 'title'),
-            (Programme, 'get_state_display'),
-            (Role, 'title'),
-            (AlternativeProgrammeForm, 'title'),
+            (Person, "surname"),
+            (Person, "first_name"),
+            (Person, "nick"),
+            (Person, "email"),
+            (Person, "phone"),
+            (Person, "may_send_info"),
+            (Programme, "title"),
+            (Programme, "get_state_display"),
+            (Role, "title"),
+            (AlternativeProgrammeForm, "title"),
         ]
 
     def get_csv_related(self):
