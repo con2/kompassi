d4d559599739679bf4881afbccb9bb1d5df3bcfa
Santtu Pajukanta <santtu@pajukanta.fi>
2023-03-17 20:21:33 +0200

    fix: navbar brand

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,13 @@
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 const Navigation = () => (
   <Navbar bg="dark" variant="dark" expand="lg" className="Navigation-Navbar">
-    <Navbar.Brand href="/">
-      Kompassi<sup>v2 BETA</sup>
-    </Navbar.Brand>
+    <Container fluid>
+      <Navbar.Brand href="/">
+        Kompassi<sup>v2 BETA</sup>
+      </Navbar.Brand>
+    </Container>
   </Navbar>
 );
 
