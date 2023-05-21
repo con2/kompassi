a8d7b17b53b98eb2b46e0c6e601d7f9bad87b848
Anssi Kolehmainen <anssi@aketzu.net>
2023-05-21 10:02:53 +0300

    Fix crash on missing Host header

--- listings/middleware.py
+++ listings/middleware.py
@@ -4,7 +4,7 @@ from django.utils.deprecation import MiddlewareMixin
 
 class ListingsMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        hostname = request.META["HTTP_HOST"]
+        hostname = request.META.get("HTTP_HOST", "")
 
         if hostname in settings.KOMPASSI_LISTING_URLCONFS:
             request.urlconf = settings.KOMPASSI_LISTING_URLCONFS[hostname]
