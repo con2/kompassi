e1611c2743c714afc89aa699719e3a9ba9c84bf7
Santtu Pajukanta <santtu@pajukanta.fi>
2023-07-31 11:00:11 +0300

    feat(tickets): add product handouts report

--- tickets/views/tickets_admin_reports_view.py
+++ tickets/views/tickets_admin_reports_view.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 
 from ..helpers import tickets_admin_required
-from ..models import Order
+from ..models import Order, ProductHandout
 
 
 @tickets_admin_required
 def tickets_admin_reports_view(request, vars, event):
     vars.update(
+        product_handouts=ProductHandout.get_product_handouts(event),
         arrivals_by_hour=Order.get_arrivals_by_hour(event),
     )
 
