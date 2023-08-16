00826d33f5dafaa7c6aca55a04e63b1a34b01156
Anssi Kolehmainen <anssi@aketzu.net>
2023-08-16 18:53:25 +0300

    shumicon2023: Add tickets

--- events/shumicon2023/management/commands/setup_shumicon2023.py
+++ events/shumicon2023/management/commands/setup_shumicon2023.py
@@ -19,7 +19,7 @@ class Setup:
         self.setup_core()
         self.setup_labour()
         self.setup_badges()
-        # self.setup_tickets()
+        self.setup_tickets()
         self.setup_programme()
         self.setup_intra()
         # self.setup_access()
@@ -48,7 +48,7 @@ class Setup:
             defaults=dict(
                 public=True,
                 name="Shumicon 2023",
-                name_genitive="Shumiconissa",
+                name_genitive="Shumiconin",
                 name_illative="Shumiconiin",
                 name_inessive="Shumiconissa",
                 homepage_url="https://shumicon.fi",
@@ -126,13 +126,13 @@ class Setup:
                 "Cosplayvänkäri",
                 "Cosplayvänkäri työskentelee tapahtumassa backstagella cosplaykisojen parissa. Työtehtäviin kuuluu mm. cosplaykisaajien avustaminen sekä ohjeistus, tuomarointien aikataulujen seuraaminen ja backstagen yleisestä viihtyvyydestä huolehtiminen. HUOM! Työvuoro sijoittuu täysin lauantaipäivälle.",
                 [tyovoima],
-            ),    
+            ),
             (
                 "erikoinen",
                 "Erikoistehtävä",
                 "Haethan erikoistehtävää vain, jos sinulle on erikseen näin ohjeistettu. Selvennäthän vapaatekstikenttään lopussa, mikä on hakemasi tehtävä.",
                 [tyovoima],
-            ),  
+            ),
             (
                 "greenroom",
                 "Greenroom",
@@ -280,6 +280,86 @@ class Setup:
                 active_until=self.event.end_time,
             ),
         )
+
+    def setup_tickets(self):
+        from tickets.models import TicketsEventMeta, LimitGroup, Product
+
+        tickets_admin_group, pos_access_group = TicketsEventMeta.get_or_create_groups(self.event, ["admins", "pos"])
+
+        defaults = dict(
+            admin_group=tickets_admin_group,
+            pos_access_group=pos_access_group,
+            due_days=14,
+            shipping_and_handling_cents=0,
+            reference_number_template="2023{:06d}",
+            contact_email="Shumicon 2023 -lipunmyynti <lipunmyynti@shumicon.fi>",
+            ticket_free_text="Tämä on sähköinen lippusi Shumicon 2023 -tapahtumaan. Sähköinen lippu vaihdetaan rannekkeeseen\n"
+            "lipunvaihtopisteessä saapuessasi tapahtumaan. Voit tulostaa tämän lipun tai näyttää sen\n"
+            "älypuhelimen tai tablettitietokoneen näytöltä. Mikäli kumpikaan näistä ei ole mahdollista, ota ylös\n"
+            "kunkin viivakoodin alla oleva neljästä tai viidestä sanasta koostuva Kissakoodi ja ilmoita se\n"
+            "lipunvaihtopisteessä.\n\n"
+            "Jos tapahtuma joudutaan perumaan, lippusi käy sellaisenaan seuraavassa Shumiconissa tai voit saada\n"
+            "hyvityksen ottamalla yhteyttä: lipunmyynti@shumicon.fi.\n\n"
+            "Tervetuloa Shumiconiin!\n\n"
+            "---\n\n"
+            "This is your electronic ticket to Shumicon 2023. The electronic ticket will be exchanged to a wrist band\n"
+            "at a ticket exchange desk on your arrival at the event. You can print this ticket or show it from the\n"
+            "screen of a smartphone or tablet. If neither is possible, please write down the four or five words from\n"
+            "beneath the bar code and show that at a ticket exchange desk.\n\n"
+            "In case the event has to be canceled your ticket will be valid for the next Shumicon or you can be\n"
+            "reimbursed for your ticket. For event cancellation related reimbursement please contact us at\n"
+            "lipunmyynti@shumicon.fi.\n\n"
+            "Welcome to Shumicon!",
+            front_page_text="<h2>Tervetuloa Shumiconin lippukauppaan! / Welcome to Shumicon's Ticket Store!</h2>"
+            "<p>Liput maksetaan oston yhteydessä Paytrailin kautta. Lippujen ostaminen vaatii suomalaiset verkkopankkitunnukset, luottokortin (Visa/Mastercard/American Express) tai MobilePayn. Pyydämme suosimaan maksamista verkkopankilla, mikäli mahdollista.</p>"
+            "<p>Maksetut liput toimitetaan e-lippuina sähköpostitse asiakkaan antamaan osoitteeseen. E-liput vaihdetaan rannekkeiksi tapahtumaan saavuttaessa.</p>"
+            "<p>Lisätietoja lipuista saat tapahtuman verkkosivuilta. <a href='https://shumicon.fi/lipunmyynti/'>Siirry takaisin tapahtuman verkkosivuille</a>.</p>"
+            "<p>---</p>"
+            "<p>The tickets are paid for at the moment of purchase via Paytrail. Purchasing a ticket requires a Finnish online banking service account, a credit card (Visa/Mastercard/American Express) or MobilePay. Paying via online banking is the most preferred option, if possible.</p>"
+            "<p>Paid tickets will be sent as e-tickets to the e-mail address provided by the customer. E-tickets will be exchanged to wristbands at the event venue.</p>"
+            "<p>More information about the tickets can be found on event website. <a href='https://shumicon.fi/lipunmyynti/'> Go back to the event's website</a>.</p>",
+        )
+
+        if self.test:
+            t = now()
+            defaults.update(
+                ticket_sales_starts=t - timedelta(days=60),
+                ticket_sales_ends=t + timedelta(days=60),
+            )
+
+        meta, unused = TicketsEventMeta.objects.get_or_create(event=self.event, defaults=defaults)
+
+        def limit_group(description, limit):
+            limit_group, unused = LimitGroup.objects.get_or_create(
+                event=self.event,
+                description=description,
+                defaults=dict(limit=limit),
+            )
+
+            return limit_group
+
+        for product_info in [
+            dict(
+                name="Shumicon 2023 viikonloppulippu / Weekend Ticket",
+                description="Sisältää pääsyn Shumicon 2023 -tapahtumaan koko viikonlopun ajan. / Includes the entrance to Shumicon 2023 for the weekend.",
+                limit_groups=[
+                    limit_group("Pääsyliput", 600),
+                ],
+                price_cents=1000,
+                requires_shipping=False,
+                electronic_ticket=True,
+                available=True,
+                ordering=self.get_ordering_number(),
+            ),
+        ]:
+            name = product_info.pop("name")
+            limit_groups = product_info.pop("limit_groups")
+
+            product, unused = Product.objects.get_or_create(event=self.event, name=name, defaults=product_info)
+
+            if not product.limit_groups.exists():
+                product.limit_groups.set(limit_groups)
+                product.save()
 
     def setup_badges(self):
         from badges.models import BadgesEventMeta
