e1144f58ccbcea23220d675ebcc0a2f7d8b140b3
Santtu Pajukanta <santtu@pajukanta.fi>
2023-10-19 15:06:47 +0300

    fix(api_v2): nope, that did not work

--- api_v2/custom_oauth2_validator.py
+++ api_v2/custom_oauth2_validator.py
@@ -9,20 +9,6 @@ logger = logging.getLogger("kompassi")
 class CustomOAuth2Validator(OAuth2Validator):
     oidc_claim_scope = None
 
-    def validate_code(self, client_id, code, client, request, *args, **kwargs):
-        """
-        Stop users with no verified email address from logging in to external apps.
-        TODO this approach does not give them a proper error message
-        """
-        if not request.user.person.is_email_verified:
-            logger.warning(
-                "User %s tried to log in via OIDC with unverified email address",
-                request.user.id,
-            )
-            return False
-
-        return super().validate_code(client_id, code, client, request, *args, **kwargs)
-
     def get_userinfo_claims(self, request):
         """
         Used by /oidc/userinfo/
