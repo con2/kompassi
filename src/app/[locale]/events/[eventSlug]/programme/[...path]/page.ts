086803b8caabb455c2e042864784352ece3d5c76
Santtu Pajukanta <santtu@pajukanta.fi>
2023-11-03 17:28:15 +0200

    feat(program): redirect programme -> program

---
+++ src/app/[locale]/events/[eventSlug]/programme/[...path]/page.ts
@@ -0,0 +1,19 @@
+import { redirect } from "next/navigation";
+
+interface ProgrammeRedirectProps {
+  params: {
+    locale: string;
+    eventSlug: string;
+    path: string[];
+  };
+}
+
+/**
+ * Redirect programme -> program
+ */
+export default function ProgrammeRedirect({ params }: ProgrammeRedirectProps) {
+  const { locale, eventSlug, path } = params;
+  return void redirect(
+    `/${locale}/events/${eventSlug}/program/${path.join("/")}`
+  );
+}
