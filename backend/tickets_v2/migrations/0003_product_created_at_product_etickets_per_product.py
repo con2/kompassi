7afc76d7f46516e2a51a18b38a14b690ef1da008
Santtu Pajukanta <santtu@pajukanta.fi>
2024-12-16 21:28:51 +0200

    chore(ruff)

--- backend/tickets_v2/migrations/0003_product_created_at_product_etickets_per_product.py
+++ backend/tickets_v2/migrations/0003_product_created_at_product_etickets_per_product.py
@@ -5,21 +5,20 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('tickets_v2', '0002_product_max_per_order'),
+        ("tickets_v2", "0002_product_max_per_order"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='product',
-            name='created_at',
+            model_name="product",
+            name="created_at",
             field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
             preserve_default=False,
         ),
         migrations.AddField(
-            model_name='product',
-            name='etickets_per_product',
+            model_name="product",
+            name="etickets_per_product",
             field=models.PositiveSmallIntegerField(default=1),
         ),
     ]
