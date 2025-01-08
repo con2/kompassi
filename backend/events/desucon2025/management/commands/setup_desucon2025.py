0b0a7178b7dd10864867263da91d435d132d2ac8
Santtu Pajukanta <santtu@pajukanta.fi>
2025-01-08 19:20:36 +0200

    chore(desucon2025): do not the program v1

--- backend/events/desucon2025/management/commands/setup_desucon2025.py
+++ backend/events/desucon2025/management/commands/setup_desucon2025.py
@@ -5,8 +5,6 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils.timezone import now
 
-from core.utils import full_hours_between
-
 
 class Setup:
     def __init__(self):
@@ -21,7 +19,6 @@ class Setup:
         self.tz = tzlocal()
         self.setup_core()
         self.setup_labour()
-        self.setup_programme()
         self.setup_access()
         self.setup_badges()
         self.setup_intra()
@@ -235,179 +232,6 @@ class Setup:
                 real_name_must_be_visible=True,
             ),
         )
-
-    def setup_programme(self):
-        from labour.models import PersonnelClass
-        from programme.models import (
-            AlternativeProgrammeForm,
-            Category,
-            ProgrammeEventMeta,
-            Role,
-            Room,
-            SpecialStartTime,
-            Tag,
-            TimeBlock,
-            View,
-        )
-
-        ProgrammeEventMeta.get_or_create_groups(self.event, ["hosts"])
-        (programme_admin_group,) = ProgrammeEventMeta.get_or_create_groups(self.event, ["admins"])
-        programme_event_meta, unused = ProgrammeEventMeta.objects.get_or_create(
-            event=self.event,
-            defaults=dict(
-                public=False,
-                admin_group=programme_admin_group,
-                contact_email="Desuconin ohjelmavastaava <ohjelma@desucon.fi>",
-            ),
-        )
-
-        personnel_class = PersonnelClass.objects.get(event=self.event, slug="ohjelma")
-        personnel_2nd_class = PersonnelClass.objects.get(event=self.event, slug="ohjelma-2lk")
-
-        role_priority = 0
-        for role_title in [
-            "Ohjelmanjärjestäjä",
-            "Näkymätön ohjelmanjärjestäjä",
-            "Panelisti",
-            "Työpajanpitäjä",
-            "Keskustelupiirin vetäjä",
-            "Tuomari",
-            "Juontaja",
-        ]:
-            role_personnel_class = personnel_class if "hjelmanjärjestäjä" in role_title else personnel_2nd_class
-
-            role, unused = Role.objects.get_or_create(
-                title=role_title,
-                personnel_class__event=self.event,
-                defaults=dict(
-                    personnel_class=role_personnel_class,
-                    is_default=role_title == "Ohjelmanjärjestäjä",
-                    is_public=role_title not in ["Näkymätön ohjelmanjärjestäjä", "Tuomari"],
-                    require_contact_info=True,
-                    priority=role_priority,
-                ),
-            )
-            role_priority += 10
-
-            # remove for frostbite2024
-            if role.personnel_class != role_personnel_class:
-                role.personnel_class = role_personnel_class
-                role.save()
-
-        Role.objects.filter(
-            personnel_class__event=self.event,
-            title="Näkymätön ohjelmanjärjestäjä",
-            override_public_title="",
-        ).update(
-            override_public_title="Ohjelmanjärjestäjä",
-        )
-
-        for title, slug, style in [
-            ("Muu ohjelma", "other", "color1"),
-            ("Paneeli", "paneeli", "color2"),
-            ("Luento", "luento", "color3"),
-            ("Keskustelupiiri", "keskustelupiiri", "color4"),
-            ("Paja", "paja", "color5"),
-            ("Pienluento", "pienluento", "color6"),
-            ("Esitys", "esit", "color7"),
-            ("Visa", "visa", "color7"),
-            ("Erikoisohjelma", "erik", "color7"),
-            ("Sisäinen ohjelma", "sisainen-ohjelma", "sisainen"),
-        ]:
-            Category.objects.update_or_create(
-                event=self.event,
-                slug=slug,
-                create_defaults=dict(
-                    title=title,
-                ),
-                defaults=dict(
-                    style=style,
-                    public=style != "sisainen",
-                    v2_dimensions={},  # force its update via .save
-                ),
-            )
-
-        form, created = AlternativeProgrammeForm.objects.get_or_create(
-            event=self.event,
-            slug="default",
-            defaults=dict(
-                title="Ohjelmalomake",
-                short_description="",
-                programme_form_code="events.desucon2025.forms:ProgrammeForm",
-                num_extra_invites=0,
-            ),
-        )
-
-        if not TimeBlock.objects.filter(event=self.event).exists():
-            saturday = self.event.start_time + timedelta(days=1)  # type: ignore
-            for start_time, end_time in [
-                (
-                    self.event.start_time.replace(hour=17, minute=0, tzinfo=self.tz),  # type: ignore
-                    saturday.replace(hour=1, minute=0, tzinfo=self.tz),
-                ),
-                (
-                    saturday.replace(hour=9, minute=0, tzinfo=self.tz),
-                    self.event.end_time.replace(hour=1, minute=0, tzinfo=self.tz),  # type: ignore
-                ),
-                (
-                    self.event.end_time.replace(hour=9, minute=0, tzinfo=self.tz),  # type: ignore
-                    self.event.end_time.replace(hour=18, minute=0, tzinfo=self.tz),  # type: ignore
-                ),
-            ]:
-                TimeBlock.objects.get_or_create(
-                    event=self.event, start_time=start_time, defaults=dict(end_time=end_time)
-                )
-
-        for time_block in TimeBlock.objects.filter(event=self.event):
-            # Half hours
-            # [:-1] – discard 18:30
-            for hour_start_time in full_hours_between(time_block.start_time, time_block.end_time)[:-1]:
-                SpecialStartTime.objects.get_or_create(
-                    event=self.event,
-                    start_time=hour_start_time.replace(minute=30),  # look, no tz
-                )
-
-        view, created = View.objects.get_or_create(
-            event=self.event,
-            name="Ohjelmakartta",
-        )
-        if created:
-            view.rooms = [
-                Room.objects.get_or_create(
-                    event=self.event,
-                    name=room_name,
-                )[0]
-                for room_name in [
-                    "Pääsali",
-                    "Kuusi",
-                    "Puuseppä",
-                    "Koivu",
-                    "Honka",
-                ]
-            ]
-            view.save()
-
-        tag_order = 0
-        for tag_title, tag_slug, tag_style in [
-            ("Cosplaypainotteinen ohjelma", "cosplay", "label-danger"),
-            ("Mangapainotteinen ohjelma", "manga", "label-success"),
-            ("Ohjelman tarkoitus on viihdyttää", "viihde", "label-primary"),
-            ("Ohjelma sisältää juonipaljastuksia", "spoilers", "label-warning"),
-            ("Ohjelma keskittyy analysoivampaan lähestymistapaan", "analyysi", "label-default"),
-            ("Animepainotteinen ohjelma", "anime", "label-info"),
-        ]:
-            Tag.objects.get_or_create(
-                event=self.event,
-                slug=tag_slug,
-                defaults=dict(
-                    title=tag_title,
-                    order=tag_order,
-                    style=tag_style,
-                ),
-            )
-            tag_order += 10
-
-        self.event.programme_event_meta.create_groups()
 
     def setup_access(self):
         from access.models import GroupPrivilege, Privilege
