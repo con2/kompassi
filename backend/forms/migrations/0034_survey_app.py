3dbb42a4e8861a0a1399b1df7f9261631ed7fe2b
Santtu Pajukanta <santtu@pajukanta.fi>
2025-01-25 18:43:39 +0200

    chore(ruff)

--- backend/forms/migrations/0034_survey_app.py
+++ backend/forms/migrations/0034_survey_app.py
@@ -4,15 +4,19 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('forms', '0033_survey_protect_responses'),
+        ("forms", "0033_survey_protect_responses"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='survey',
-            name='app',
-            field=models.CharField(choices=[('forms', 'Forms V2'), ('program_v2', 'Program V2')], default='forms', help_text='Which app manages this survey?', max_length=10),
+            model_name="survey",
+            name="app",
+            field=models.CharField(
+                choices=[("forms", "Forms V2"), ("program_v2", "Program V2")],
+                default="forms",
+                help_text="Which app manages this survey?",
+                max_length=10,
+            ),
         ),
     ]
