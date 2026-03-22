e94e0287edd2c21823620cb99be64ed949e68e8b
Uuno Tapper <47410836+uunotap@users.noreply.github.com>
2026-03-23 01:53:56 +0200

    Working event page

--- kompassi/events/finncon2026/management/commands/setup_finncon2026.py
+++ kompassi/events/finncon2026/management/commands/setup_finncon2026.py
@@ -87,8 +87,15 @@ class Setup:
         from kompassi.labour.models.personnel_class import PersonnelClass
         from kompassi.labour.models.job_category import JobCategory
 
-        from ...models import SignupExtra
-
+        from ...models import SignupExtra, SpecialDiet
+        from kompassi.labour.models import (
+            AlternativeSignupForm,
+            InfoLink,
+            JobCategory,
+            LabourEventMeta,
+            PersonnelClass,
+            Qualification,
+        )
 
 
         (labour_admin_group,) = LabourEventMeta.get_or_create_groups(self.event, ["admins"])
@@ -119,6 +126,13 @@ class Setup:
 
 
 
+        for diet_name in ["Gluteeniton", "Laktoositon", "Maidoton", "Vegaaninen", "Lakto-ovo-vegetaristinen"]:
+            SpecialDiet.objects.get_or_create(name=diet_name)
+
+
+
+
+
 
         ## organizers -- vastaavat PersonnelClass
         organizers_personnel_class, created = PersonnelClass.objects.get_or_create(
@@ -132,6 +146,7 @@ class Setup:
         )
 
 
+        ## Vapaaehtoinen työkategoria
         volunteer_pc, _ = PersonnelClass.objects.get_or_create(
             event=self.event,
             slug="vapaaehtoinen",
@@ -144,48 +159,41 @@ class Setup:
 
 
 
-
-
         ## Vapaaehtoinen työkategoria
-        voluntary_gategory, v_created = JobCategory.objects.update_or_create(
-            event=self.event,
-            slug="vapaaehtoinen",
-            defaults=dict(
-                name="Vänkäri (Vapaaehtoinen)",
-                description="Tapahtuman vapaaehtoistehtävät eri osa-alueilla.",
-                public=True,
-            ),
-        )
-        if v_created:
-            voluntary_gategory.personnel_classes.set([volunteer_pc])
-
-        ## Järjestyksenvalvoja vetäminen ja asettaminen -- jobcategory mitälie
+#        voluntary_gategory, v_created = JobCategory.objects.update_or_create(
+#            event=self.event,
+#            slug="vapaaehtoinen",
+#            defaults=dict(
+#                name="Vänkäri (Vapaaehtoinen)",
+#                description="Tapahtuman vapaaehtoistehtävät eri osa-alueilla.",
+#                public=True,
+#            ),
+#        )
+#        if v_created:
+#            voluntary_gategory.personnel_classes.set([volunteer_pc])
 
 
-
-        valvoja, j_created = JobCategory.objects.update_or_create(
-            event=self.event,
-            slug="valvoja",
-            defaults=dict(
-                name="Järjestyksenvalvoja",
-                description="Järjestyksenvalvoja...?",
-                public=True,
-            ),
-        )
-
-        if j_created:
-            valvoja.personnel_classes.set([volunteer_pc])
-            qual = Qualification.objects.get(name="JV-kortti")
-            valvoja.required_qualifications.set([qual])
-
-
-
-        ## taika koodi???
-#        for jc_name, qualification_name in [
-#            ("Järjestyksenvalvoja", "JV-kortti"),
-#        ]:
-#            JobCategory.objects.get(event=self.event, name=jc_name)
-#            Qualification.objects.get(name=qualification_name)
+        for slug, name, description in [
+            ("vankari", "Yleisvänkäri", "Valmiina auttamaan siellä, missä tarvitaan. Yleisvänkärille hyviä ominaisuuksia ovat esim. oma-aloitteisuus. Yleisvänkäreitä sijoitetaan tapahtuman aikana eri puolille conia ja toiveita oman sijoittumisen suhteen saa esittää. Yleisvänkärille voi tulla myös päivystysvuoro Vänkäriluolassa, jolloin siellä huolehditaan samalla tarjoilujen ylläpidosta ja tilan siisteydestä, sekä lähdetään tarvittaessa sinne missä apua kaivataan."),
+            ("vankarigreen", "Taukotilavänkäri", "Löytyykö sinulta hygieniapassi, ruokapuolen osaamista tai intoa oppia näitä taitoja? Haluatko olla varmistamassa, että henkilökunta pysyy virkeänä ja hyvin ravittuna con-päivän ajan? Hae osaksi taukotilatiimiä, joka huolehtii tapahtuman aikana Greenroomin, Vänkäriluolan ja näytteilleasetajien taukopaikan kahvi- ja välipalatatjoiluista, sekä näiden tilojen siisteydestä, kasauksesta ja purusta. Hygieniapassi ei ole pakollinen, mutta yleinen osaaminen ymmärrys elintarviketurvallisuudesta ja esimerkiksi hyvästä käsihygieniasta on. Muita hyviä ominaisuuksia tehtävään ovat omatoimisuus, järjestelmällisyys ja toimeliaisuus, sekä sosiaaliset taidot vuorovaikutettaessa niin taukotilaan levähtämään tulevan henkilökunnan kuin oman tiiminkin kanssa."),
+            ("vankariinfo", "Infovänkäri", "Omaatko hyvät vuorovaikutustaidot ja “ellen tiedä niin ei hätää, minä selvitän!” -asenteen? Hae osaksi Finnconin tuikitärkeää infotiimiä huolehtimaan, että niin kävijät kuin henkilökuntakin pysyvät informoituina ja löytävät oikeaan aikaan tiensä perille. Info kokoaa myös sinne tuodut löytötavarat ja luovuttaa ne kyselijöille tavaran tuntomerkkejä vastaan."),
+            ("vankarinarikka", "Narikkavänkäri", "Narikassa autat varmistamaan, että kävijöiden isommat kantamukset pysyvät turvassa tapahtuman ajan. Selkeä tehtävä, jossa pärjää omatessaan hyvät vuorovaikutustaidot, sekä ripauksen käytännöllistä loogisuutta."),
+            ("vankaripurku", "Purku-/rakennusvänkäri", "Haluatko nauttia itse tapahtumasta rauhassa ja antaa työpanoksesi ennen sen alkua ja/tai tapahtuman päätyttyä? Tule osaksi kasaus- ja purkutiimiä! Hommiin sisältyy monenmoista roudailua, joten kohtuullinen fyysinen kunto ja ergonomiset työtavat ovat tässä tehtävässä paikallaan."),
+            ("vankariohjelma", "Ohjelmavänkäri", "Haluaisitko toimia ohjelmapitäjien apuna luentosalissa tai työpajassa? Tehtävässä hyödyksi ovat avulias asenne(jos esimerkiksi pöytiä tai tuoleja pitää järjestää uudelleen tai yleisön kysyjälle viedä mikrofoni), salivänkärillä tekninen osaaminen. Pajavänkäreillä myös käsityöharrastuneisuus voi olla eduksi työpajojen vetäjiä autettaessa."),
+            ("tiiminvetaja", "Tiiminvetäjä", "Finncon on iso tapahtuma ja vapaaehtoisia yleensä paljon. Tehtävien jaon helpottamiseksi haussa on myös tiiminvetäjiä toimimaan oman vastuualueensa vänkäreiden tukena tapahtuman aikana yhteistyössä työvoimavastaavan kanssa. Tehtävässä keskeisiä taitoja ovat hyvät vuorovaikutustaidot ja ongelmaratkaisukyky, järjestelmällisyys, aktiivinen ote hommiin ja itsenäinen päätöksentekokyky."),
+        ]:
+            jc, created = JobCategory.objects.update_or_create(
+                event=self.event,
+                slug=slug,
+                defaults=dict(
+                    name=name,
+                    app_label="vapaaehtoinen",
+                    description=description,
+                    public=True,
+                ),
+            )
+            if created:
+                jc.personnel_classes.set([volunteer_pc])
 
 
 
@@ -204,6 +212,47 @@ class Setup:
         if created:
             conitea_job_category.personnel_classes.set([organizers_personnel_class])
 
+
+
+
+        ## taika koodi??? ## Vaatii aiemman iteraation
+#        for jc_name, qualification_name in [
+#            ("Järjestyksenvalvoja", "JV-kortti"),
+#        ]:
+#            JobCategory.objects.get(event=self.event, name=jc_name)
+#            Qualification.objects.get(name=qualification_name)
+
+        valvoja, j_created = JobCategory.objects.update_or_create(
+            event=self.event,
+            slug="valvoja",
+            defaults=dict(
+                name="Järjestyksenvalvoja",
+                description='JV-kortti taskussa? Tehtävässä vaaditaan hyviä vuorovaikutustaitoja ja tilannetajua, sekä rauhallisen jämäkkää otetta. Jos et tällä hetkellä omista JV-korttia ja olet sen suorittamassa sen koulutuksen ennen tapahtumaa, mainitse asiasta "Vapaa alue" tekstikentässä!',
+                public=True,
+            ),
+        )
+
+        if j_created:
+            valvoja.personnel_classes.set([volunteer_pc])
+            qual = Qualification.objects.get(name="JV-kortti")
+            valvoja.required_qualifications.set([qual])
+
+
+
+
+
+
+        AlternativeSignupForm.objects.get_or_create(
+            event=self.event,
+            slug="conitea",
+            defaults=dict(
+                title="Conitean ilmoittautumislomake",
+                signup_form_class_path="events.finncon2026.forms:OrganizerSignupForm",
+                signup_extra_form_class_path="events.finncon2026.forms:OrganizerSignupExtraForm",
+                active_from=now(),
+                active_until=self.event.start_time,
+            ),
+        )
 
 
 
