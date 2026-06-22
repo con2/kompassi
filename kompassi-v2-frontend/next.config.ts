a056496d95285db515d56283ebdcf8fab750e8fe
Luka Pajukanta <santtu@pajukanta.fi>
2026-06-22 15:58:42 +0300

    fix: big request bodies in v2 frontend

--- kompassi-v2-frontend/next.config.ts
+++ kompassi-v2-frontend/next.config.ts
@@ -1,11 +1,14 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
+const bodySizeLimit = "100mb";
+
 const nextConfig: NextConfig = {
   output: "standalone",
   experimental: {
+    middlewareClientMaxBodySize: bodySizeLimit,
     serverActions: {
-      bodySizeLimit: "100mb",
+      bodySizeLimit,
     },
   },
   sassOptions: {
