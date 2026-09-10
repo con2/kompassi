0f7d318f98636a0cf47c95499b7ef0fb2124ac84
Luka Pajukanta <santtu@pajukanta.fi>
2026-09-10 08:15:51 +0300

    feat(intra): show sudo views to superuser on intra admin permission denied

--- kompassi/intra/helpers.py
+++ kompassi/intra/helpers.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect
 from django.utils.translation import gettext_lazy as _
 
+from kompassi.access.cbac import default_cbac_required, get_default_claims, raise_cbac_permission_denied
 from kompassi.core.models import Event
 from kompassi.core.utils import login_redirect
 
@@ -39,8 +40,10 @@ def intra_organizer_required(view_func):
             return login_redirect(request)
 
         if not meta.is_user_allowed_to_access(request.user):
-            messages.error(request, _("You do not have access to the organizer intranet."))
-            return redirect("core_event_view", event.slug)
+            # while this access check is not CBAC, we raise a CBAC permission denied
+            # to show sudo view to superuser
+            claims = get_default_claims(request)
+            raise_cbac_permission_denied(request, claims)
 
         if meta.is_user_admin(request.user):
             is_intra_admin = True
@@ -65,19 +68,13 @@ def intra_organizer_required(view_func):
 
 def intra_admin_required(view_func):
     @wraps(view_func)
+    @default_cbac_required
     def wrapper(request, event_slug, *args, **kwargs):
         event = get_object_or_404(Event, slug=event_slug)
         meta = event.intra_event_meta
 
         if not meta:
             messages.error(request, _("This event does not use the organizer intranet."))
-            return redirect("core_event_view", event.slug)
-
-        if not request.user.is_authenticated:
-            return login_redirect(request)
-
-        if not meta.is_user_admin(request.user):
-            messages.error(request, _("You do not have administrator access to the organizer intranet."))
             return redirect("core_event_view", event.slug)
 
         menu_items = intra_admin_menu_items(request, event)
